95f7d035dc5172cc2c881b5e3f00f47439980a05
Subhojit Roy <subhojitr@corp.eoxvantage.com>
2025-10-13 14:12:59 +0530

    doing from scratch-fixing html response fixing 7

--- health-check.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-// Clean any buffered output
-if (function_exists('ob_get_level')) { while (ob_get_level() > 0) { ob_end_clean(); } }
-
-header('Content-Type: application/json; charset=utf-8');
-echo json_encode(['ok' => true, 'ping' => 'pong', 'ts' => time()]);
-exit;
